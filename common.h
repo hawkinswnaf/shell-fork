5301250c5d48805caf4ebfbb0e66b81a75210c81
Will Hawkins <hawkinsw@opentechinstitute.org>
2012-08-02 13:52:34 -0400

    DEBUG_[0-3] Levels
    Add debug levels so it's not all or nothing
    debugging information.

--- common.h
+++ common.h
@@ -1,6 +1,50 @@
 #ifndef _COMMON_H 
 #define _COMMON_H 
 
-#define DEBUG printf
+/*
+ * start of debugging output configuration.
+ */
+#define DEBUG_LEVEL 3
+//#define DEBUG_LEVEL 2
+//#define DEBUG_LEVEL 1
+//#define DEBUG_LEVEL 0
+
+#if DEBUG_LEVEL >= 3
+#define DEBUG_3 printf
+#else
+#define DEBUG_3  
+#endif
+
+#if DEBUG_LEVEL >= 2
+#define DEBUG_2 printf
+#else
+#define DEBUG_2  
+#endif 
+
+#if DEBUG_LEVEL >= 1
+#define DEBUG_1 printf
+#else
+#define DEBUG_1  
+#endif 
+
+#ifndef DEBUG_LEVEL
+#define DEBUG_1  
+#define DEBUG_2  
+#define DEBUG_3  
+#else
+#define DEBUG_MODE
+#endif
+
+/*
+ * end of debugging output configuration.
+ */
+
+#define GLOBAL_OUTPUT
+
+#define LINE_BUFFER_SIZE 80
+
+#define READ_END 0
+#define WRITE_END 1
+#define GLOBAL_PIPE_UNINITIALIZED -1
 
 #endif
